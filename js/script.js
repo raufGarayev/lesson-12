77c0fed5684ca3429f85ad2989d544389c32978c
GARAYEV-PG <rafkagarayev@gmail.com>
2023-11-20 22:33:57 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let firstName = "Cavid"
+
+firstName = "Rauf"
+
+const lastName = "Məmmədli"
+
+
+//QETI SEKILDE ISLENILMESIN
+/* var surname = "Ehmedli"
+
+var surname = "Abbasov" */
+
+const fullName = firstName + " " + lastName
+
